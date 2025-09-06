7c2111aec3a4a1c38481e141738bf464496db7a6
HazelAmi <dalafeghaquakumey@byupathway.edu>
2025-09-06 15:36:09 +0000

    homepage edits

--- scripts/course.js
+++ scripts/course.js
@@ -7,7 +7,7 @@ const courses = [
     certificate: 'Web and Computer Programming',
     description: 'This course will introduce students to programming...',
     technology: ['Python'],
-    completed: false
+    completed: true
   },
   {
     subject: 'WDD',
@@ -17,7 +17,7 @@ const courses = [
     certificate: 'Web and Computer Programming',
     description: 'This course introduces students to the World Wide Web...',
     technology: ['HTML', 'CSS'],
-    completed: false
+    completed: true
   },
   {
     subject: 'CSE',
@@ -27,7 +27,7 @@ const courses = [
     certificate: 'Web and Computer Programming',
     description: 'CSE 111 students become more organized...',
     technology: ['Python'],
-    completed: false
+    completed: true
   },
   {
     subject: 'CSE',
@@ -37,7 +37,7 @@ const courses = [
     certificate: 'Web and Computer Programming',
     description: 'This course will introduce the notion of classes...',
     technology: ['C#'],
-    completed: false
+    completed: true
   },
   {
     subject: 'WDD',
@@ -47,7 +47,7 @@ const courses = [
     certificate: 'Web and Computer Programming',
     description: 'This course builds on prior experience...',
     technology: ['HTML', 'CSS', 'JavaScript'],
-    completed: false
+    completed: true
   },
   {
     subject: 'WDD',
@@ -61,41 +61,7 @@ const courses = [
   }
 ];
 
-// function renderCourses(filter = "all") {
-//   const container = document.getElementById("course-cards");
-//   container.innerHTML = "";
 
-//   const filtered = filter === "all"
-//     ? courses
-//     : courses.filter(course => course.subject.toLowerCase() === filter);
-
-//   let totalCredits = 0;
-
-//   filtered.forEach(course => {
-//     const card = document.createElement("div");
-//     card.className = "course-card" + (course.completed ? " completed" : "");
-//     card.innerHTML = `
-//       <h3>${course.subject} ${course.number}</h3>
-//       <p><strong>${course.title}</strong></p>
-//       <p>${course.description}</p>
-//       <p><strong>Credits:</strong> ${course.credits}</p>
-//       <p><strong>Technologies:</strong> ${course.technology.join(', ')}</p>
-//     `;
-//     container.appendChild(card);
-//     totalCredits += course.credits;
-//   });
-
-//   document.getElementById("total-credits").textContent = totalCredits;
-// }
-
-// document.querySelectorAll(".filters button").forEach(button => {
-//   button.addEventListener("click", () => {
-//     renderCourses(button.dataset.filter);
-//   });
-// });
-
-// Initial render
-//renderCourses();
 
 const courseList = document.querySelector('.course-cards');
 const totalCredits = document.getElementById('total-credits');
